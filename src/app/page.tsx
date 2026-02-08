454998e0701f3e679d33b175144c6bc44482f5dc
mafutka <mafutka@gmail.com>
2026-02-08 17:30:39 +0100

    header-home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Header from "./components/Header/Header";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-       
-      </main>
+    <div >
+      <Header />
+      
     </div>
   );
 }
